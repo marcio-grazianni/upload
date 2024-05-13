6df4cef077a2191005642d0398667e42eab5fa58
marcio <marcio.grazianni@gmail.com>
2024-05-13 17:08:56 -0300

    Novas melhorias estéticas.

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.urls.resolvers import URLPattern
 from app import views
 
-
-urlpatterns: list[URLPattern] = [
-    path(route='upload/', view=views.file_upload, name='file_upload'),
+urlpatterns = [
+    path(route='', view=views.file_upload, name='file_upload'),
 ]
