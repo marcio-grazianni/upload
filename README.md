969b74192345392d19c6c6f7dadb046fdc3ff063
marcio <marcio.grazianni@gmail.com>
2024-05-14 18:07:04 -0300

    Start processing csv.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # UPLOAD
 ## Python 3.12.3
-## Django 5.0.4
+## Django 5.0.6
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from app.forms import UploadFileForm
@@ -9,15 +11,40 @@ def principal(request) -> HttpResponse:
 
 def file_upload(request) -> HttpResponse:
     lines: list = []
+    info: dict = {}
     if request.method == 'POST':
-        form = UploadFileForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            for line in request.FILES['arquivo']:
-                if line.decode('utf-8').strip():
-                    lines.append(line.decode('utf-8').strip())
-                    print(line.decode('utf-8').strip())
+        formulario = UploadFileForm(data=request.POST, files=request.FILES)
+        arquivo = request.FILES['arquivo']
 
-            return render(request=request, template_name='upload_complete.html', context={'lines': lines})
+        # Variáveis para colocar no dicionário "info"
+        nome_completo_arquivo = arquivo.name
+        nome_arquivo, extensao_arquivo = os.path.splitext(nome_completo_arquivo)
+        info['nome_completo_arquivo'] = nome_completo_arquivo
+        info['nome_arquivo'] = nome_arquivo
+        info['extensao_arquivo'] = extensao_arquivo.replace(".", "")
+        print("-" * 100)
+        for key, value in info.items():
+            print(f'{key}: {value}')
+        if info['extensao_arquivo'] == "csv":
+            print("Extensão csv detectada.")
+        print("-" * 100)
+        if formulario.is_valid():
+            start_csv_data = False
+            for line in arquivo:
+                line = line.decode('utf-8').strip()
+                if line:
+                    if info['extensao_arquivo'] == "csv":
+                        if not start_csv_data and "Data" in line:
+                            start_csv_data = True
+                        if start_csv_data:
+                            lines.append(line)
+                            print(line)
+                    else:
+                        lines.append(line)
+                        print(line)
+            print("-" * 100)
+
+            return render(request=request, template_name='upload_complete.html', context={'lines': lines, 'info': info})
     else:
-        form = UploadFileForm()
-    return render(request=request, template_name='upload.html', context={'form': form})
+        formulario = UploadFileForm()
+    return render(request=request, template_name='upload.html', context={'form': formulario})
